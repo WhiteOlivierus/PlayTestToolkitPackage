448d4c1920cfc6cb3b9dd9bd8e1c3fc9969e75c0
WhiteOlivierus <bas.dijkstra@student.hku.nl>
2021-01-14 16:21:55 +0100

    Tot play test data kan je navigeren

---
+++ PlayTestTookitWeb/PlayTestBuildsAPI/Models/DataFile - Copy.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace PlayTestBuildsAPI.Models
+{
+    public class DataFile
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string ConfigId { get; set; }
+
+        public double StartTime { get; set; }
+
+        public IList<InputObject> Input { get; set; } = new List<InputObject>();
+    }
+}
---
+++ PlayTestTookitWeb/PlayTestWebUI/Pages/Playtest.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using PlayTestWebUI.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PlayTestWebUI.Pages
+{
+    public partial class Playtest : ComponentBase
+    {
+        [Parameter] public string Title { get; set; }
+
+        private IList<DataFile> Data { get; set; } = new List<DataFile>();
+
+        private bool HasData { get => Data.Any(); }
+
+        protected override async Task OnInitializedAsync() => await RefreshProjects();
+
+        private async Task RefreshProjects()
+        {
+            using HttpClient client = new HttpClient
+            {
+                BaseAddress = new("http://PlayTestBuildsAPI:80")
+            };
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.GetAsync($"/api/data/{Title}");
+
+            if (!response.IsSuccessStatusCode)
+                return;
+
+            Stream stream = await response.Content.ReadAsStreamAsync();
+            Data = await JsonSerializer.DeserializeAsync<IList<DataFile>>(stream);
+        }
+    }
+}
